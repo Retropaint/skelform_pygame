c3606912f273a26951ed1ac52f9c71c747f1418f
Retropaint <darkglasses1122@gmail.com>
2026-01-05 03:05:10 +0700

    added comments and typehints

--- skelform_pygame/__init__.py
+++ skelform_pygame/__init__.py
@@ -12,9 +12,11 @@ from typing import Optional
 import skelform_python as skf_py
 import dacite
 import pygame
+from typing import List, Tuple
 
 
-def load(path):
+# Loads an `.skf` file.
+def load(path: str) -> Tuple[skf_py.Armature, List[pygame.image]]:
     with zipfile.ZipFile(path, "r") as zip_file:
         armature_json = json.load(zip_file.open("armature.json"))
 
@@ -29,33 +31,39 @@ def load(path):
 
 
 @dataclass
-class AnimOptions:
+class ConstructOptions:
     position: pygame.math.Vector2
     scale: pygame.Vector2
-    blend_frames: list[int]
 
     def __init__(
         self,
         position=pygame.Vector2(0, 0),
         scale=pygame.Vector2(0.25, 0.25),
-        blend_frames=[0, 0, 0, 0, 0, 0],
     ):
         self.position = position
         self.scale = scale
-        self.blend_frames = blend_frames
 
 
-# Animate a SkelForm armature.
+# Transforms an armature's bones based on the provided animation(s) and their frame(s).
+#
+# `smoothFrames` is used to smoothly interpolate transforms. Mainly used for smooth animation transitions. Higher frames are smoother.
+#
+# Note: smoothFrames should ideally be set to 0 (or empty) when reversing animations.
 def animate(
-    armature,
+    armature: skf_py.Armature,
     animations: list[skf_py.Animation],
     frames: list[int],
-    blend_frames: list[int],
-):
-    return skf_py.animate(armature, animations, frames, blend_frames)
+    smooth_frames: list[int],
+) -> List[skf_py.Bone]:
+    return skf_py.animate(armature, animations, frames, smooth_frames)
 
 
-def construct(armature, screen, anim_options):
+# Returns the constructed array of bones from this armature.
+#
+# While constructing, several options (positional offset, scale) may be set.
+def construct(
+    armature: skf_py.Armature, screen: pygame.Surface, anim_options: ConstructOptions
+) -> List[skf_py.Bone]:
     final_bones = skf_py.construct(armature)
 
     for bone in final_bones:
@@ -70,17 +78,26 @@ def construct(armature, screen, anim_options):
     return final_bones
 
 
-def draw(props, styles, tex_imgs, screen):
-    props.sort(key=lambda prop: prop.zindex)
+# Draws the bones to the provided screen, using the provided styles and textures.
+#
+# Recommended: include the whole texture array from the file even if not all will be used,
+# as the provided styles will determine the final appearance.
+def draw(
+    bones: List[skf_py.Bone],
+    styles: List[skf_py.Style],
+    tex_imgs: List[pygame.image],
+    screen: pygame.Surface,
+):
+    bones.sort(key=lambda prop: prop.zindex)
     surfaces = []
 
-    final_textures = skf_py.setup_bone_textures(props, styles)
+    final_textures = skf_py.setup_bone_textures(bones, styles)
 
-    for prop in props:
-        if prop.id not in final_textures:
+    for bone in bones:
+        if bone.id not in final_textures:
             continue
 
-        tex = final_textures[prop.id]
+        tex = final_textures[bone.id]
 
         tex_surf = tex_imgs[tex.atlas_idx].subsurface(
             (tex.offset.x, tex.offset.y, tex.size.x, tex.size.y)
@@ -90,20 +107,20 @@ def draw(props, styles, tex_imgs, screen):
 
         tex_surf = pygame.transform.smoothscale_by(
             tex_surf,
-            (math.fabs(prop.scale.x), math.fabs(prop.scale.y)),
+            (math.fabs(bone.scale.x), math.fabs(bone.scale.y)),
         )
 
-        if prop.scale.x < 0 or prop.scale.y < 0:
+        if bone.scale.x < 0 or bone.scale.y < 0:
             tex_surf = pygame.transform.flip(
-                tex_surf, prop.scale.x < 0, prop.scale.y < 0
+                tex_surf, bone.scale.x < 0, bone.scale.y < 0
             )
 
         # push textures back left and up so that it's centered
-        prop_tex_pos = prop.pos
+        prop_tex_pos = bone.pos
         prop_tex_pos.x -= tex_surf.get_size()[0] / 2
         prop_tex_pos.y -= tex_surf.get_size()[1] / 2
 
-        deg = math.degrees(prop.rot)
+        deg = math.degrees(bone.rot)
         (tex_surf, rect) = rot_center(tex_surf, tex_surf.get_rect(), deg)
 
         moved_rect = rect.move(
@@ -117,15 +134,17 @@ def draw(props, styles, tex_imgs, screen):
 
 
 # https://www.pygame.org/wiki/RotateCenter
-def rot_center(image, rect, angle):
+def rot_center(image: pygame.image, rect: pygame.Rect, angle: float):
     rot_image = pygame.transform.rotate(image, angle)
     rot_rect = rot_image.get_rect(center=rect.center)
     return rot_image, rot_rect
 
 
-def time_frame(time, animation: skf_py.Animation, reverse, loop):
+# Returns the animation frame based on the provided time.
+def time_frame(time: int, animation: skf_py.Animation, reverse: bool, loop: bool):
     return skf_py.time_frame(time, animation, reverse, loop)
 
 
-def format_frame(frame, animation: skf_py.Animation, reverse, loop):
+# Returns the properly bound animation frame based on the provided animation.
+def format_frame(frame: int, animation: skf_py.Animation, reverse: bool, loop: bool):
     return skf_py.format_frame(frame, animation, reverse, loop)
